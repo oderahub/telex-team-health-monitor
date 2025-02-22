81ca03a5a180510860c3406251ab0cee41944cb8
oderahub <okere_chidera@yahoo.com>
2025-02-22 18:43:27 +0100

    chore: add corn job

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import express from 'express'
+import https from 'https'
 import { config } from 'dotenv'
 import wellnessRouter from './routes/wellness'
 import schedule from 'node-schedule'
+import cron from 'node-cron'
 
 config()
 
@@ -18,3 +20,19 @@ app.use('/api', wellnessRouter)
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+function keepAlive(url: string) {
+  https
+    .get(url, (res) => {
+      console.log(`Status: ${res.statusCode}`)
+    })
+    .on('error', (error) => {
+      console.error(`Error: ${error.message}`)
+    })
+}
+
+// Schedule a job to keep the server alive
+cron.schedule('*/5 * * * *', () => {
+  keepAlive('')
+  console.log('Pinged the server every 5 minutes')
+})
